4c5756d6bdc7004214c1f83484932d95d97e02c3
Borko Stankovic <borkostankovic83@yahoo.com>
2025-11-12 11:36:22 -0600

    widgetyocale

--- parts/home1/coming-soon.php
+++ parts/home1/coming-soon.php
@@ -4,7 +4,43 @@
 </div>
 <div class="container" style="margin-bottom: 2rem;">
   <div id="widget-placeholder"></div>
+
   <script>
+//   (function() {
+//     var s = document.createElement('script');
+//     s.type = 'text/javascript';
+//     s.async = true;
+//     s.src = 'https://app.yocale.com/tentacle/tentacle.v1.0.0.js';
+//     var x = document.getElementsByTagName('script')[0];
+//     x.parentNode.insertBefore(s, x);
+//     s.onload = s.onreadystatechange = function() {
+//       if (!this.readyState || this.readyState === 'complete') {
+//         Tentacle.run({
+//           business: 'coastal-bliss-wellness',
+//           type:'INLINE-WIDGET',
+//           label:'Book Appointment',
+//           labelColor:'#FFFFFF',         // Button text color
+//           iconColor:'#FFFFFF',          // Icon color
+//           backgroundColor:'#536DFE',    // Button background color
+//           disableIcon:false,
+//           targetSelector:'#widget-placeholder',
+//           buttonStyles: {
+//             // You can add more CSS properties here, for example:
+//             borderRadius: '8px',
+//             border: '2px solid #beac5a',
+//             background: '#beac5a',
+//             color: '#fff'
+//           }
+//         });
+//       }
+//     };
+//   })();
+//   </script>
+</div>
+<div style="margin-top: 20px"></div>
+    <div class="embedsocial-hashtag" data-ref="2082f57e6c1dfcf1aee3e702453c811610349aac"> <a class="feed-powered-by-es feed-powered-by-es-slider-img es-widget-branding" href="https://embedsocial.com/social-media-aggregator/" target="_blank" title="Instagram widget"> <img src="https://embedsocial.com/cdn/icon/embedsocial-logo.webp" alt="EmbedSocial"> <div class="es-widget-branding-text">Instagram widget</div> </a> </div> <script> (function(d, s, id) { var js; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = "https://embedsocial.com/cdn/ht.js"; d.getElementsByTagName("head")[0].appendChild(js); }(document, "script", "EmbedSocialHashtagScript")); </script>
+</div>
+<script>
   (function() {
     var s = document.createElement('script');
     s.type = 'text/javascript';
@@ -16,34 +52,15 @@
       if (!this.readyState || this.readyState === 'complete') {
         Tentacle.run({
           business: 'coastal-bliss-wellness',
-          type:'INLINE-WIDGET',
+          type:'FLOATING',
           label:'Book Appointment',
-          labelColor:'#FFFFFF',         // Button text color
-          iconColor:'#FFFFFF',          // Icon color
-          backgroundColor:'#536DFE',    // Button background color
+          labelColor:'#FFFFFF',
+          iconColor:'#FFFFFF',
+          backgroundColor:'#9CD948',
           disableIcon:false,
-          targetSelector:'#widget-placeholder',
-          buttonStyles: {
-            // You can add more CSS properties here, for example:
-            borderRadius: '8px',
-            border: '2px solid #beac5a',
-            background: '#beac5a',
-            color: '#fff'
-          }
+          buttonStyles:{}
         });
       }
     };
   })();
   </script>
-</div>
-<div style="margin-top: 20px"></div>
-  <!-- <div class="container"> -->
-
-    <!-- <div class="modal-body" style="height: 700px; padding: 0;">
-      <iframe src="https://www.yocale.com/widget/coastal-bliss-wellness?locations=165570"
-              width="100%" height="100%" frameborder="0" style="border: none;"></iframe>
-    </div> -->
-    <div class="embedsocial-hashtag" data-ref="2082f57e6c1dfcf1aee3e702453c811610349aac"> <a class="feed-powered-by-es feed-powered-by-es-slider-img es-widget-branding" href="https://embedsocial.com/social-media-aggregator/" target="_blank" title="Instagram widget"> <img src="https://embedsocial.com/cdn/icon/embedsocial-logo.webp" alt="EmbedSocial"> <div class="es-widget-branding-text">Instagram widget</div> </a> </div> <script> (function(d, s, id) { var js; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = "https://embedsocial.com/cdn/ht.js"; d.getElementsByTagName("head")[0].appendChild(js); }(document, "script", "EmbedSocialHashtagScript")); </script>
-
-  <!-- </div> -->
-</div> 
